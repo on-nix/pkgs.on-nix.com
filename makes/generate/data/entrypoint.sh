3a7170de9bbd7b93136e680aa0a432b4a8cd990d
Kevin Amado <kamadorueda@gmail.com>
2021-10-29 23:40:10 -0500

    feat(front): single layout

--- makes/generate/data/entrypoint.sh
+++ makes/generate/data/entrypoint.sh
@@ -8,8 +8,11 @@ function main {
     && for attr_path in __argData__/nixpkgs/*; do
       : && attr="$(basename "${attr_path}")" \
         && attr="${attr%.*}" \
+        && set="$(dirname "${attr_path}")" \
+        && set="$(basename "${set}")" \
         && echo --- > "content/nixpkgs/${attr}.md" \
         && echo "attr: ${attr}" >> "content/nixpkgs/${attr}.md" \
+        && echo "set: ${set}" >> "content/nixpkgs/${attr}.md" \
         && echo --- >> "content/nixpkgs/${attr}.md"
     done \
     && cp -rLT __argData__/nixpkgs data/nixpkgs \
