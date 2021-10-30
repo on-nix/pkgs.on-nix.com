d6739a10765124849ccfcacf5ec56c708fd53069
Kevin Amado <kamadorueda@gmail.com>
2021-10-29 23:15:22 -0500

    feat(cross): attrs

--- makes/generate/data/entrypoint.sh
+++ makes/generate/data/entrypoint.sh
@@ -1,10 +1,17 @@
 # shellcheck shell=bash
 
 function main {
-  : && rm -rf content/* \
-    && rm -rf data/* \
+  : && rm -rf content \
+    && rm -rf data \
     && mkdir -p content/nixpkgs \
     && mkdir -p data/nixpkgs \
+    && for attr_path in __argData__/nixpkgs/*; do
+      : && attr="$(basename "${attr_path}")" \
+        && attr="${attr%.*}" \
+        && echo --- > "content/nixpkgs/${attr}.md" \
+        && echo "attr: ${attr}" >> "content/nixpkgs/${attr}.md" \
+        && echo --- >> "content/nixpkgs/${attr}.md"
+    done \
     && cp -rLT __argData__/nixpkgs data/nixpkgs \
     && :
 }
